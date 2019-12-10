87a585f8f7336a5dc8f97434ac3d80ee3e900693
He11B1azzzer <faster.113544.lost@gmail.com>
2019-12-10 14:42:04 +0200

    for dead only

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import func as fn
 from PySide2 import QtCore, QtGui, QtWidgets
 import sys
 from untitled import Ui_MainWindow
+import os.path
 
 app = QtWidgets.QApplication(sys.argv)
 MainWindow = QtWidgets.QMainWindow()
@@ -22,6 +23,7 @@ def chek(string):
     else:
         return False
 
+
 def draw():
     dice = 0
     sides = 0
@@ -34,10 +36,21 @@ def draw():
         probs = []
         targets = [i for i in range(dice, dice*sides + 1)]
         for i in range(len(targets)):
-            probs.append(fn.probability(dice,sides,targets[i])) 
-        plt.figure(1)
+            probs.append(fn.probability(dice,sides,targets[i]))
+        filename = str(dice) + "d" + str(sides) + "s" + ".txt"
+        if os.path.exists(os.path.join(os.path.abspath(os.path.dirname(sys.argv[0])), ".\logs",filename)) == False:
+            f = open(os.path.join(os.path.abspath(os.path.dirname(sys.argv[0])), ".\logs",filename), "w")
+            for i in range(len(probs)):
+                f.write("\n")
+                f.write(str(targets[i]))
+                f.write("             ")
+                f.write(str(probs[i]))
+                f.write("\n")
+            f.close()
+        plt.figure("График для числа кубиков {0} с числом сторон равным {1} каждый".format(dice,sides))
         plt.bar(targets,probs)
         plt.show()
+
 
 ui.pushButton.clicked.connect(draw)
 sys.exit(app.exec_())
--- untitled.py
+++ untitled.py
@@ -3,7 +3,7 @@
 # Form implementation generated from reading ui file 'E:\Grand\Coding\Python\prob_dice_mylovechuck\untitled.ui',
 # licensing of 'E:\Grand\Coding\Python\prob_dice_mylovechuck\untitled.ui' applies.
 #
-# Created: Sat Dec  7 00:20:04 2019
+# Created: Mon Dec  9 22:58:22 2019
 #      by: pyside2-uic  running on PySide2 5.13.2
 #
 # WARNING! All changes made in this file will be lost!
@@ -35,16 +35,29 @@ class Ui_MainWindow(object):
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 413, 21))
         self.menubar.setObjectName("menubar")
+        self.menuFile = QtWidgets.QMenu(self.menubar)
+        self.menuFile.setObjectName("menuFile")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
+        self.actionSave = QtWidgets.QAction(MainWindow)
+        self.actionSave.setObjectName("actionSave")
+        self.actionExit = QtWidgets.QAction(MainWindow)
+        self.actionExit.setObjectName("actionExit")
+        self.menuFile.addAction(self.actionSave)
+        self.menuFile.addSeparator()
+        self.menuFile.addAction(self.actionExit)
+        self.menubar.addAction(self.menuFile.menuAction())
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QtWidgets.QApplication.translate("MainWindow", "Броски кубика", None, -1))
+        MainWindow.setWindowTitle(QtWidgets.QApplication.translate("MainWindow", "MainWindow", None, -1))
         self.pushButton.setText(QtWidgets.QApplication.translate("MainWindow", "Нарисовать график", None, -1))
         self.label.setText(QtWidgets.QApplication.translate("MainWindow", "Количество кубиков", None, -1))
         self.label_2.setText(QtWidgets.QApplication.translate("MainWindow", "Количество сторон кубика", None, -1))
+        self.menuFile.setTitle(QtWidgets.QApplication.translate("MainWindow", "File", None, -1))
+        self.actionSave.setText(QtWidgets.QApplication.translate("MainWindow", "Save", None, -1))
+        self.actionExit.setText(QtWidgets.QApplication.translate("MainWindow", "Exit", None, -1))
